1cce9ce11fc42dcc283954eb889aa1e8caf5c562
wwyyzz <sdn_app@163.com>
2020-01-06 17:25:32 +0800

    1-6

--- src/main/java/com/wj/MAC_Query/controller/macaddrController.java
+++ src/main/java/com/wj/MAC_Query/controller/macaddrController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+
 public class macaddrController {
     final static String INVALID = "NOT A VALID MAC ADDRESS";
     private Logger logger = LoggerFactory.getLogger(MacQueryApplication.class);
