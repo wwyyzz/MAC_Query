125ca282cdc27621d483c8e9b11944f44d8df75f
wwyyzz <sdn_app@163.com>
2020-01-09 12:06:17 +0800

    gitignore

--- src/main/java/com/wj/MAC_Query/DAO/macaddrRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wj.MAC_Query.DAO;
-
-import com.wj.MAC_Query.entity.macAddr;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface macaddrRepository extends JpaRepository<macAddr, Integer> {
-     macAddr findByMacaddr(String macAddr);
-}
